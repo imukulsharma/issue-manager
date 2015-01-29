072bcd381b6c4ce8288ec386fd3cbe6de0edb76c
Mukul Sharma <mukuls@fractalanalytics.com>
2015-01-29 16:01:14 +0530

    Adding Mobile

--- src/main/java/learningspringboot/IssueManager.java
+++ src/main/java/learningspringboot/IssueManager.java
@@ -3,17 +3,30 @@ package learningspringboot;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.social.github.api.GitHubIssue;
 import org.springframework.social.github.api.impl.GitHubTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class IssueManager {
-	String githubToken = "f1b671760dfd5e43f3de6278b62e023102328fa5";
-	String org = "spring-projects";
-	String[] repos = new String[] { "spring-boot", "spring-boot-issues" };
+public class IssueManager implements InitializingBean {
 
-	GitHubTemplate gitHubTemplate = new GitHubTemplate(githubToken);
+	@Value("${github.token}")
+	String githubToken;
+
+	@Value("${org}")
+	String org;
+
+	@Value("${repos}")
+	String[] repos;
+
+	GitHubTemplate gitHubTemplate;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		gitHubTemplate = new GitHubTemplate(githubToken);
+	}
 
 	public List<Issue> findOpenIssues() {
 		List<Issue> openIssues = new ArrayList<Issue>();
